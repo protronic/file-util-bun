6a442c37f732e035a68eaf879e8d4c705e7c4fc2
Robert Seidler <r.seidler@protronic-gmbh.de>
2024-07-14 16:47:48 +0200

    added build options for linux;

--- src/rm.ts
+++ src/rm.ts
@@ -1,23 +1,6 @@
 import { rm, rmdir, stat } from "node:fs/promises";
 
-// async function removeFile(target: string){
-
-// }
-
-// async function removeDir(target: string, recursive: boolean){
-  
-// }
-
 export async function remove(target: string, recursive: boolean) {
-  // try{
-  //   if((await stat(target)).isDirectory()){
-  //     return await removeDir(target, recursive);
-  //   } else {
-  //     return await removeFile(target);
-  //   }
-  // } catch(err) {
-  //   console.error(err);
-  // }
   try {
     await rm(target, {recursive: recursive, force: true});
   } catch(err) {
